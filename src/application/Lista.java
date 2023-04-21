bc871d4369a0f5027454de9c8a3470112480446b
Henrique Kriguer <henriquekriguer@gmail.com>
2023-04-21 12:33:52 -0300

    Classe iterator  da interface List

--- src/application/Lista.java
+++ src/application/Lista.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class Lista {
@@ -60,38 +61,85 @@ public class Lista {
 		notas.add(7.7);
 		notas.add(9.0);
 		notas.add(2.5);
+		notas.add(0d);
 		
 	System.out.println(notas);
 	
-	/*	
+	/*	// 2 - Para adicionar um elemento na lista em uma posição determinada :
+	 * 
 		notas.add(3, 0.5);
 		
 		System.out.println(notas);
-// Exiba a posição da  nota 9.0
+		
+		
+//       3 - Exiba a posição da  nota 9.0
 		
 		System.out.println(notas.indexOf(9.0));
 		
-//Substitua a nota 9.0 pela nota 9.5 nesta posição.
+//       4 - Substitua a nota 9.0 pela nota 9.5 nesta posição.
 		
 		notas.set(notas.indexOf(9.0), 9.5);
 		
 		System.out.println(notas);
 		
-// Confira se a nota 2.5 esta presente na coleção:
+//       5 - Confira se a nota 2.5 esta presente na coleção:
 		
 		System.out.println (notas.contains(2.5));
 		
-// Impressão dos itens da lista na vertical:
+//       6- Impressão dos itens da lista na vertical:
 		
 		for ( Double nota :  notas) {
 			System.out.println(nota);
 		}
 		
 */		
-		
-// Executar um método para ordenar em ordem crescente a lista:
-		Collections.sort(notas);
-		System.out.println(notas);
+	
+//      7 - Obtenha o Quarto elemento da lista.
+	
+//	System.out.println(notas.get(3));
+	
+//     8 - Exiba a maior e a menor nota da lista:
+	
+//System.out.println("A maior nota é: " + Collections.max(notas));
+//System.out.println("A menor nota é: " + Collections.min(notas));
+
+//     9 - Determine a média das notas:
+// A - fazendo a soma dos elementos:
+/*
+Iterator<Double> iterator = notas.iterator();
+Double soma = 0d;
+	while (iterator.hasNext()) {
+		Double next = iterator.next();
+		soma += next;
+	}
+	
+	
+// B - Obtendo o número de elementos:
+Integer qtd = notas.size();
+Double quantidade = (double)qtd;
+
+// C - Obtendo a média aritimética:
+
+Double mediaAritmetica = soma / quantidade;
+
+System.out.println("A média Aritimética é:" + mediaAritmetica);
+
+	
+//     10 - Executar um método para ordenar em ordem crescente a lista:
+//		Collections.sort(notas);
+//		System.out.println(notas);
+ */
+ 
+ 
+ //      11 - Removendo um elemento em uma determinada posição:
+ 
+ 		notas.remove(0);
+ 		System.out.println(notas);
+ 		
+ 		//12 - Removendo um elemento da lista :
+ 		
+ 			notas.remove(0d);
+ 		System.out.println(notas);
 	}
 
 }
